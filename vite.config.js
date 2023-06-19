9ba292aa32a7160d5363002ff95a7510aaf8ba75
Chandan Singh <chandan@kongsbergdigital.com>
2023-06-19 15:11:18 +0530

    Removed unused dynamic import reference

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
-import dynamicImportVars from '@rollup/plugin-dynamic-import-vars';
 
 export default defineConfig({
   base: '/portfolio/',
